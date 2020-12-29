35be28640efe606abe5eb35174076b669ba35273
Łukasz Królak <lukasz.krolak@gmail.com>
2020-12-29 11:26:29 +0100

    prepare for d'n'd /learning

---
+++ my-app/src/components/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import '../index.css'
+import { _getPhones } from '../utils/_DATA'
+import { receivePhones } from '../actions/Phones'
+import Container from './container'
+import 'semantic-ui-css/semantic.min.css'
+
+class App extends Component {
+  componentDidMount(){
+    _getPhones()
+      .then((phones) =>{
+        this.props.dispatch(receivePhones(phones))
+      })
+  }
+  render(){
+    const { phones } = this.props
+  
+    return (
+      <Container phones={phones} />
+    );
+  }
+}
+
+function mapStateToProps({phones}){
+  return{
+    phones
+  }
+}
+
+export default connect(mapStateToProps)(App);
